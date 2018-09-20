65386687707dc91eec4739b7e8602537c9aaed71
DmitriySav <decky1994@gmail.com>
2018-09-20 22:30:11 +0300

    Добавьте файлы проекта.

---
+++ SignalRPlusAzureQueue/Readers/QueueReader.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace SignalRPlusAzureQueue.Readers
+{
+    public class QueueReader
+    {
+        private string _queueName;
+        private CloudQueue _queue;
+        public QueueReader(string queueName)
+        {
+            _queueName = queueName;
+        }
+        public void ConnectToQueue()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+               CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            _queue = queueClient.GetQueueReference(_queueName);
+        }
+        public int QueueCount()
+        {
+            _queue.FetchAttributes();
+            var count = _queue.ApproximateMessageCount;
+            return count.GetValueOrDefault();
+        }
+        public string GetMessage()
+        {
+            CloudQueueMessage queueMessage = _queue.GetMessage();
+            _queue.DeleteMessage(queueMessage);
+            return queueMessage.AsString;
+        }
+        
+    }
+}
