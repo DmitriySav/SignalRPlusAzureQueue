997c869a63144a4b2e349e054bea4de746d9a5ad
Dmitrij <decky1994@gmail.com>
2018-09-25 16:07:55 +0300

    write new tests for reaader which tested events

--- tests/Siognalr.Tests/QueueReaderTests.cs
+++ tests/Siognalr.Tests/QueueReaderTests.cs
@@ -24,18 +24,50 @@ namespace Siognalr.Tests
         }
 
         [Test]
-        public void QueueReader_GetMessage_eventCall()
+        public void GetMessage_WhenCalled_IvokeEventCall()
         {
+            //arrange
             _queueService.Setup(x => x.GetMessage()).Returns("Message");
             queueReader = new QueueReader(_queueService.Object);
             bool messageEvent = false;
-
-
             queueReader.OnGetMessage += (e) => messageEvent = true;
+
             //act
             queueReader.GetMessage();
 
+            //assert
             Assert.IsTrue(messageEvent);
+        }
+
+        [Test]
+        public void GetMessage_WhenCalled_IvokeEventCall_MessageIsHello()
+        {
+            //arrange
+            _queueService.Setup(x => x.GetMessage()).Returns("Hello");
+            queueReader = new QueueReader(_queueService.Object);
+            string message = string.Empty;
+
+            queueReader.OnGetMessage += (s) => message = s;
+            
+            //act
+            queueReader.GetMessage();
+
+            //assert
+            Assert.AreEqual("Hello", message);
+        }
+
+        [Test]
+        public void Count_WhenCalled_ReturnOne()
+        {
+            //arrange
+            _queueService.Setup(x => x.QueueCount()).Returns(1);
+            queueReader = new QueueReader(_queueService.Object);
+
+            //act
+            int num = queueReader.Count();
+
+            //assert
+            Assert.AreEqual(num,1);
         }
     }
 }
