d0491126fe58569545e094632240d6d0bd2dbcb4
Dmitrij <decky1994@gmail.com>
2018-10-16 14:55:34 +0300

    add bearer provider

---
+++ MessageConsumer/Providers/BearerProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+
+namespace MessageConsumer.Providers
+{
+    public class BearerProvider:OAuthBearerAuthenticationProvider
+    {
+        public override Task RequestToken(OAuthRequestTokenContext context)
+        {
+           // base.RequestToken(context);
+            var value = context.Request.Query.Get("Authorization");
+
+            if (!string.IsNullOrEmpty(value))
+                context.Token = value;            
+            
+            return Task.FromResult<object>(null);
+        }
+    }
+}
